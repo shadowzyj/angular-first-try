d5318b71254d0ad61457820fc983bd325e3cd5fa
shadowzyj <976547914@qq.com>
2020-12-02 09:44:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-at6rqe-imvkw3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-at6rqe-imvkw3)
---
+++ src/app/products.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    name: "XiaoMi 8",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    name: "RedMi K30 Pro",
+    price: 65,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    name: "RedMi K30 Pro Plus",
+    price: 299,
+    description: ""
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
